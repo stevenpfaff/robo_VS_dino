44983e620c5d8023f5852589c8b62dc64e14c21b
stevenpfaff <89651503+stevenpfaff@users.noreply.github.com>
2021-09-09 14:42:52 -0500

    Add files via upload

--- Robots Vs Dinosaurs/fleet.py
+++ Robots Vs Dinosaurs/fleet.py
@@ -1,16 +1,9 @@
 from robot import Robot
+from weapon import Weapon
 
 class Fleet:
-    def __init__(self) -> None:
-        self.name = []
-        self.add_robot_to_herd()
-
-    def add_robot_to_herd(self):
-        name1 = Robot("IG-88")
-        name2 = Robot("General Greivous")
-        name3 = Robot("C3PO")
-
-        self.name.append(name1)
-        self.name.append(name2)
-        self.name.append(name3)
-
+    def __init__(self):
+        pass
+        
+    def create_fleet(self):
+        pass
