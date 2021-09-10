37c7fee27a947706b04cbe9257d2f50a1100500e
stevenpfaff <89651503+stevenpfaff@users.noreply.github.com>
2021-09-10 08:25:42 -0500

    Update battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -1,3 +1,27 @@
+from fleet import Fleet
+from herd import Herd
+
 class Battlefield:
     def __init__(self):
-        self.type = "City"
+        pass
+
+    def run_game(self):
+        pass
+    
+    def display_welcome(self):
+        print("Welcome to Robots Versus Dinosaurs")
+
+    def dino_turn(self, dinosaur):
+        pass
+
+    def robot_turn(self, robot):
+        pass
+
+    def show_dino_opponent_options(self):
+        pass
+    
+    def show_robot_opponent_options(self):
+        pass
+    
+    def display_winner(self):
+        print("Robo/Dino Wins!")
