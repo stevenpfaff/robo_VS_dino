158ac7176e294f635503f9639f4f915bc38d6e3d
stevenpfaff <89651503+stevenpfaff@users.noreply.github.com>
2021-09-10 08:24:25 -0500

    Update herd.py

--- herd.py
+++ herd.py
@@ -1,3 +1,6 @@
 from dino import Dinosaur
 class Herd:
     def __init__(self):
+        self.dino_one = Dinosaur("Monty")
+        self.dino_two = Dinosaur("Rex")
+        self.dino_three = Dinosaur("Scoop") 
