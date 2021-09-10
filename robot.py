8a4ec3a1ceb9058019408b341a24213817c2370e
stevenpfaff <89651503+stevenpfaff@users.noreply.github.com>
2021-09-10 09:52:14 -0500

    Update robot.py

--- robot.py
+++ robot.py
@@ -1,8 +1,13 @@
-class Weapon:
-    def __init__(self) -> None:
-        self.weapon = ["Lightsaber", "Blaster", "Laser"]
-        self.attack_power = 110
-        
+from weapon import Weapon
+
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.weapon = Weapon("Lightsaber", 80)
+
+    def attack(self, dinosaur):
+        dinosaur.health -= self.weapon.attack_power    
 
     
         
