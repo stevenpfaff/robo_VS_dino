5f3239e6881452037af6a90859e7cec4f1e1d738
stevenpfaff <89651503+stevenpfaff@users.noreply.github.com>
2021-09-10 09:53:59 -0500

    Update battlefield.py

--- battlefield.py
+++ battlefield.py
@@ -3,24 +3,25 @@ from herd import Herd
 
 class Battlefield:
     def __init__(self):
-        pass
+        self.fleet = Fleet()
+        self.herd = Herd()
 
     def run_game(self):
         pass
     
     def display_welcome(self):
         print("Welcome to Robots Versus Dinosaurs")
-        
+
     def show_dino_opponent_options(self):
         print(Herd())
     
     def show_robot_opponent_options(self):
-        print(Fleet())
+        print(Fleet)
 
-    def dino_turn(self, dinosaur):
+    def dino_turn(self, Dinosaur):
         pass
 
-    def robot_turn(self, robot):
+    def robot_turn(self, Robot):
         pass
     
     def display_winner(self):
